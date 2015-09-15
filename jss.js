4d52254a74b012a9b5e46cf7ab38a778531f4f5c
Lee Crossley <leee@hotmail.co.uk>
2015-09-15 14:32:06 +0100

    Fix IE8 unexpected call by style.styleSheet

--- jss.js
+++ jss.js
@@ -18,6 +18,7 @@
   }
 
   function injectCSS(css) {
+    css = css.join('\n\n');
     var style = document.getElementById('jss-styles');
     if (!style) {
       style = document.createElement('style');
@@ -26,8 +27,12 @@
       var head = document.getElementsByTagName('head')[0];
       head.insertBefore(style, head.firstChild);
     }
-    var node = document.createTextNode(css.join('\n\n'));
-    style.appendChild(node);
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css;
+    } else {
+      var node = document.createTextNode(css);
+      style.appendChild(node);
+    }
   }
 
   if (typeof exports === 'object')
